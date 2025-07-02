5d6a4cb90a8064d683a060121765dfc18884f141
Petar0803 <petar.rakadzhiyski.po27@pgmtplovdiv.info>
2025-07-02 09:06:35 +0200

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3 };
+            int[] nums2 = nums;
+            nums[0] = 0;
+
+            Console.WriteLine("Contents of nums2:");
+            Console.WriteLine(string.Join(", ", nums2));
+
+            Console.WriteLine("Loop through nums2:");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"\t{nums2[i]}");
+            }
+        }
+    }
+}
+
+
+
+
+
